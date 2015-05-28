89c63cf7d1ce37b63913337ae4ff823213f50b31
bringmehome <bringmehome@vip.qq.com>
2015-05-28 13:39:11 +0800

    ok?

---
+++ html/bak/jscss/all.js
@@ -0,0 +1,20 @@
+document.write('<script type="text/javascript" src="../script/jquery-2.1.3.js"></script>');
+document.write('<script type="text/javascript" src="../script/jquery.mobile-1.4.5.min.js"></script>');
+document.write('<script type="text/javascript" src="../script/av-0.5.0.min.js"></script>');
+document.write('<script type="text/javascript" src="../script/config.js"></script>');
+document.write('<script type="text/javascript" src="../script/api.js"></script>');
+document.write('<script type="text/javascript" src="../script/mico-bind-0.1.js"></script>');
+document.write('<script type="text/javascript" src="../script/mico-user-0.1.js"></script>');
+document.write('<script type="text/javascript" src="../script/usermanage.js"></script>');
+document.write('<script type="text/javascript" src="../script/micokitmanage.js"></script>');
+
+
+/*以下是原生的引用方法*/
+// <script type="text/javascript" src="../script/jquery-2.1.3.js"></script>
+// <script type="text/javascript" src="../script/jquery.mobile-1.4.5.min.js"></script>
+// <script type="text/javascript" src="../script/av-0.5.0.min.js"></script>
+// <script type="text/javascript" src="../script/config.js"></script>
+// <script type="text/javascript" src="../script/api.js"></script>
+// <script type="text/javascript" src="../script/mico-bind-0.1.js"></script>
+// <script type="text/javascript" src="../script/mico-user-0.1.js"></script>
+// <script type="text/javascript" src="../script/usermanage.js"></script>
